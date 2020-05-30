861aac0596313cb43989eb127ea0dd1bad3c60ff
tv-store <66169050+tv-store@users.noreply.github.com>
2020-05-30 15:58:09 +0300

    Add files via upload

---
+++ init.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+
+    $('a[href^="#"]').click(function () {
+        var elementClick = $(this).attr("href");
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated), body:not(:animated)").animate({scrollTop: destination}, 600);
+        return false;
+    });
+
+
+    $('.slider').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 960,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    adaptiveHeight: true
+                }
+            }
+        ]
+    });
+
+
+    /* timer */
+
+    function update() {
+        var Now = new Date(), Finish = new Date();
+        Finish.setHours(23);
+        Finish.setMinutes(59);
+        Finish.setSeconds(59);
+        if (Now.getHours() === 23 && Now.getMinutes() === 59 && Now.getSeconds === 59) {
+            Finish.setDate(Finish.getDate() + 1);
+        }
+        var sec = Math.floor((Finish.getTime() - Now.getTime()) / 1000);
+        var hrs = Math.floor(sec / 3600);
+        sec -= hrs * 3600;
+        var min = Math.floor(sec / 60);
+        sec -= min * 60;
+        $(".timer .hours").html(pad(hrs));
+        $(".timer .minutes").html(pad(min));
+        $(".timer .seconds").html(pad(sec));
+        setTimeout(update, 200);
+    }
+
+    function pad(s) {
+        s = ("00" + s).substr(-2);
+        return "<span>" + s[0] + "</span><span>" + s[1] + "</span>";
+    }
+
+    update();
+});
